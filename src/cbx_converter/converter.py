ad502f76568908f70e0af3bca18847bbec376d56
bcroix <brice.croix@take2games.com>
2026-09-10 13:32:59 +0200

    Add epub conversion

--- src/cbx_converter/converter.py
+++ src/cbx_converter/converter.py
@@ -268,6 +268,16 @@ def cbx_convert(
                                     ),
                                     img,
                                 )
+                case "epub":
+                    images_filenames_out_absolute = [
+                        os.path.join(output_tempdir, image_filename_out)
+                        for image_filename_out in images_filenames_out
+                    ]
+                    create_epub(
+                        images_filenames_out_absolute,
+                        output,
+                        os.path.splitext(os.path.basename(output))[0],
+                    )
                 case _:
                     raise RuntimeError(f'Unsupported output format "{output_ext}"')
             return ConvertResult.Converted
@@ -276,3 +286,79 @@ def cbx_convert(
                 return ConvertResult.Skipped
             shutil.copyfile(input, output)
             return ConvertResult.Copied
+
+
+# This method was taken from https://github.com/g0ldyy/sushiscan-downloader.git
+def create_epub(images: list[str], output_path: str, title: str):
+    """Creates epub file from given images
+
+    Parameters
+    ----------
+    images : list[str]
+        All images to put in epub file
+    output_path : str
+        Path of file to be created
+    title : str
+        The title of the epub
+    """
+    if len(images) == 0:
+        return
+    with zipfile.ZipFile(output_path, "w") as zf:
+        zf.writestr("mimetype", "application/epub+zip")
+        zf.writestr(
+            "META-INF/container.xml",
+            """<?xml version="1.0"?>
+<container version="1.0" xmlns="urn:oasis:names:tc:opendocument:xmlns:container">
+  <rootfiles>
+    <rootfile full-path="content.opf" media-type="application/oebps-package+xml"/>
+  </rootfiles>
+</container>""",
+        )
+
+        manifest = []
+        spine = []
+
+        for i, img in enumerate(images):
+            filename = os.path.basename(img)
+            zf.write(img, f"OEBPS/images/{filename}")
+            page_id = f"page_{i + 1}"
+            manifest.append(
+                f'<item id="{page_id}" href="images/{filename}" media-type="image/jpeg"/>'
+            )
+
+            html_content = f"""<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head><title>Page {i + 1}</title></head>
+<body><img src="images/{filename}" style="max-width:100%;"/></body>
+</html>"""
+            zf.writestr(f"OEBPS/page_{i + 1}.xhtml", html_content)
+            manifest.append(
+                f'<item id="xhtml_{i + 1}" href="page_{i + 1}.xhtml" media-type="application/xhtml+xml"/>'
+            )
+            spine.append(f'<itemref idref="xhtml_{i + 1}"/>')
+
+        content_opf = f"""<?xml version="1.0" encoding="utf-8"?>
+<package xmlns="http://www.idpf.org/2007/opf" unique-identifier="BookId" version="2.0">
+  <metadata xmlns:dc="http://purl.org/dc/elements/1.1/">
+    <dc:title>{title}</dc:title>
+  </metadata>
+  <manifest>
+    {"".join(manifest)}
+    <item id="ncx" href="toc.ncx" media-type="application/x-dtbncx+xml"/>
+  </manifest>
+  <spine toc="ncx">
+    {"".join(spine)}
+  </spine>
+</package>"""
+        zf.writestr("OEBPS/content.opf", content_opf)
+
+        zf.writestr(
+            "OEBPS/toc.ncx",
+            f"""<?xml version="1.0" encoding="UTF-8"?>
+<ncx xmlns="http://www.daisy.org/z3986/2005/ncx/" version="2005-1">
+  <head><meta name="dtb:uid" content="urn:uuid:12345"/></head>
+  <docTitle><text>{title}</text></docTitle>
+  <navMap/>
+</ncx>""",
+        )
