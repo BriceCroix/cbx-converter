90ce1625d8007957fbc710f337982287da7a1911
bcroix <brice.croix@gmail.com>
2026-09-15 13:00:09 +0200

    Only import pyside if necessary

--- src/cbx_converter/__main__.py
+++ src/cbx_converter/__main__.py
@@ -1,24 +1,23 @@
 import sys
 
-try:
-    from PySide6 import QtWidgets
-
-    from .gui import MyWidget
-
-    _has_pyside = True
-except ImportError():
-    _has_pyside = False
-
 from . import cli
 
 
 def main():
-    if _has_pyside and len(sys.argv) == 1:
-        app = QtWidgets.QApplication([])
-        widget = MyWidget()
-        widget.resize(800, 600)
-        widget.show()
-        sys.exit(app.exec())
+    if len(sys.argv) == 1:
+        try:
+            from PySide6 import QtWidgets
+
+            from .gui import MyWidget
+
+            app = QtWidgets.QApplication([])
+            widget = MyWidget()
+            widget.resize(800, 600)
+            widget.show()
+            sys.exit(app.exec())
+        except ImportError() as _:
+            cli.main()
+
     else:
         cli.main()
 
